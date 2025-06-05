f341bd84e49bf6931366f63b79acbdebebe1fe74
masamoto yamashiro <masamoto@protonmail.com>
2025-06-05 10:08:51 +0900

    Add files via upload

--- js/constants.js
+++ js/constants.js
@@ -5,22 +5,27 @@ const Types = {
   SYMBOL: 'symbol',
   VECTOR: 'vector',
   NIL: 'nil',
+  COMMENT: 'comment',
   UNDEFINED: 'undefined'
 };
 const ColorTypes = {
   'green': Types.NUMBER,
-  'yellow': Types.BOOLEAN,
+  'cyan': Types.BOOLEAN,
   'blue': Types.STRING,
   'red': Types.SYMBOL,
   'purple': Types.VECTOR,
+  'orange': Types.NIL,
+  'yellow': Types.COMMENT,
   'gray': Types.NIL
 };
 const colorCodes = {
   'red': '#FF4B00',
   'green': '#03AF7A',
   'blue': '#005AFF',
-  'yellow': '#FFC107',
+  'cyan': '#4DC4FF',
   'purple': '#9C27B0',
+  'orange': '#F6AA00',
+  'yellow': '#FFF100',
   'gray': '#9E9E9E'
 };
 const CONFIG = {
